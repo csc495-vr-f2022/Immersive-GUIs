c1a561a09fec1db071b71f86e89cce1d9a2618b7
rkopper <kopper@uncg.edu>
2022-12-01 15:20:13 -0500

    Revert "Completed class code"
    This reverts commit b174780e28ce3431a3e27c9a9504ebd68535b71e.

---
+++ README.md
@@ -0,0 +1,8 @@
+# Immersive GUIs
+
+## Topics
+
+1. 2D vs 3D graphical user interfaces
+2. Adding a static text tooltip to the controller
+3. Creating a button in 3D space
+4. Using panels to layout more complex GUIs
---
+++ src/index.ts
@@ -0,0 +1,148 @@
+/* CSCI 5619 Lecture 19, Fall 2020
+ * Author: Evan Suma Rosenberg
+ * License: Creative Commons Attribution-NonCommercial-ShareAlike 4.0 International
+ */ 
+
+import { Engine } from "@babylonjs/core/Engines/engine";
+import { Scene } from "@babylonjs/core/scene";
+import { Vector3, Color3 } from "@babylonjs/core/Maths/math";
+import { UniversalCamera } from "@babylonjs/core/Cameras/universalCamera";
+import { WebXRInputSource } from "@babylonjs/core/XR/webXRInputSource";
+import { WebXRCamera } from "@babylonjs/core/XR/webXRCamera";
+import { PointLight } from "@babylonjs/core/Lights/pointLight";
+import { Logger } from "@babylonjs/core/Misc/logger";
+import { GUI3DManager } from "@babylonjs/gui/3D/gui3DManager"
+import { Button3D } from "@babylonjs/gui/3D/controls/button3D"
+import { HolographicButton } from "@babylonjs/gui/3D/controls/holographicButton"
+import { TextBlock } from "@babylonjs/gui/2D/controls/textBlock"
+import { StandardMaterial } from "@babylonjs/core/Materials/standardMaterial";
+import { Animation } from "@babylonjs/core/Animations/animation"
+import { TransformNode } from "@babylonjs/core/Meshes/transformNode";
+import { MeshBuilder } from "@babylonjs/core";
+import { AdvancedDynamicTexture } from "@babylonjs/gui/2D/advancedDynamicTexture"
+import { StackPanel3D } from "@babylonjs/gui/3D/controls/stackPanel3D"
+import { PlanePanel } from "@babylonjs/gui/3D/controls/planePanel"
+import { CylinderPanel } from "@babylonjs/gui/3D/controls/cylinderPanel"
+import { SpherePanel } from "@babylonjs/gui/3D/controls/spherePanel"
+import { ScatterPanel } from "@babylonjs/gui/3D/controls/scatterPanel"
+
+// Side effects
+import "@babylonjs/core/Helpers/sceneHelpers";
+import "@babylonjs/inspector";
+
+class Game 
+{ 
+    private canvas: HTMLCanvasElement;
+    private engine: Engine;
+    private scene: Scene;
+
+    private xrCamera: WebXRCamera | null; 
+    private leftController: WebXRInputSource | null;
+    private rightController: WebXRInputSource | null;
+
+    constructor()
+    {
+        // Get the canvas element 
+        this.canvas = document.getElementById("renderCanvas") as HTMLCanvasElement;
+
+        // Generate the BABYLON 3D engine
+        this.engine = new Engine(this.canvas, true); 
+
+        // Creates a basic Babylon Scene object
+        this.scene = new Scene(this.engine);   
+
+        this.xrCamera = null;
+        this.leftController = null;
+        this.rightController = null;
+    }
+
+    start() : void 
+    {
+        // Create the scene and then execute this function afterwards
+        this.createScene().then(() => {
+
+            // Register a render loop to repeatedly render the scene
+            this.engine.runRenderLoop(() => { 
+                this.update();
+                this.scene.render();
+            });
+
+            // Watch for browser/canvas resize events
+            window.addEventListener("resize", () => { 
+                this.engine.resize();
+            });
+        });
+    }
+
+    private async createScene() 
+    {
+        // This creates and positions a first-person camera (non-mesh)
+        var camera = new UniversalCamera("camera1", new Vector3(0, 1.6, 0), this.scene);
+        camera.fov = 90 * Math.PI / 180;
+        camera.minZ = .1;
+        camera.maxZ = 100;
+
+        // This attaches the camera to the canvas
+        camera.attachControl(this.canvas, true);
+
+       // Create a point light
+       var pointLight = new PointLight("pointLight", new Vector3(0, 2.5, 0), this.scene);
+       pointLight.intensity = 1.0;
+       pointLight.diffuse = new Color3(.25, .25, .25);
+
+        // Creates a default skybox
+        const environment = this.scene.createDefaultEnvironment({
+            createGround: true,
+            groundSize: 100,
+            skyboxSize: 50,
+            skyboxColor: new Color3(0, 0, 0)
+        });
+
+        // Make sure the ground and skybox are not pickable!
+        environment!.ground!.isPickable = false;
+        environment!.skybox!.isPickable = false;
+
+        // Creates the XR experience helper
+        const xrHelper = await this.scene.createDefaultXRExperienceAsync({});
+
+        // Assigns the web XR camera to a member variable
+        this.xrCamera = xrHelper.baseExperience.camera;
+
+        // Remove default teleportation
+        xrHelper.teleportation.dispose();
+
+
+        // Assign the left and right controllers to member variables
+        xrHelper.input.onControllerAddedObservable.add((inputSource) => {
+            if(inputSource.uniqueId.endsWith("right"))
+            {
+                this.rightController = inputSource;
+            }
+            else 
+            {
+                this.leftController = inputSource;
+            }  
+        });
+
+        // Don't forget to deparent objects from the controllers or they will be destroyed!
+        xrHelper.input.onControllerRemovedObservable.add((inputSource) => {
+
+        });
+
+
+
+        this.scene.debugLayer.show(); 
+    }
+
+    // The main update loop will be executed once per frame before the scene is rendered
+    private update() : void
+    {
+ 
+    }
+
+}
+/******* End of the Game class ******/   
+
+// start the game
+var game = new Game();
+game.start();
